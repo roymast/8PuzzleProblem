15d0445252c572c0e01d9cd1691ce2b5cd64156d
roymast <roy.mastov@gmail.com>
2024-01-28 22:16:45 +0200

    limiting grid size

--- Assets/_Scripts/GridSizeSelector.cs
+++ Assets/_Scripts/GridSizeSelector.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UI;
+using UnityEngine.Purchasing;
 
 public class GridSizeSelector : MonoBehaviour
 {
+    public int MaxGrid = 13;
+    public int MinGrid = 3;
+
     public static int GridSize = 3;
     public TextMeshProUGUI GridSizeText;
+    public Button IncButton;
+    public Button DecButton;
     private void Start()
     {
-        if(GridSizeText != null)
-            GridSizeText.text = GridSize.ToString();
+        UpdateUI();
     }
     public void IncreaseGrid()
     {
-        GridSize++;
-        GridSizeText.text = GridSize.ToString();
+        if (GridSize < MaxGrid)
+            GridSize++;
+        UpdateUI();
     }
     public void DecreaseGrid()
     {
-        if(GridSize > 3)
+        if (GridSize > MinGrid)
             GridSize--;
+        UpdateUI();
+    }
+    void UpdateUI()
+    {
+        if (GridSizeText != null)
+            UpdateText();
+        if (IncButton != null && DecButton != null)
+            UpdateInteractable();
+    }
+    void UpdateText()
+    {
         GridSizeText.text = GridSize.ToString();
+    }
+    void UpdateInteractable()
+    {
+        DecButton.interactable = GridSize > MinGrid;
+        IncButton.interactable = GridSize < MaxGrid;
     }
 }
